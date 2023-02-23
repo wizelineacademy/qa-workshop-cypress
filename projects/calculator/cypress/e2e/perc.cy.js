8f1e4755c5d61fde56c6a43c1fdb9f12f7937fb1
Cristina Hernandez <A00830775@tec.mx>
2023-02-23 11:28:28 -0600

    Agrego prueba de porcentaje

---
+++ projects/calculator/cypress/e2e/perc.cy.js
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+    it('passes', () => {
+      cy.visit('http://localhost:3000/')
+      cy.contains('5').click()
+      cy.contains('0').click()
+      cy.contains('%').click()
+      cy.get('.component-display').first().should('have.text', '0.5')
+    })
+})
