e1e24b9ff87f9744708f9f31ad4b4a6e680b198c
Macias Romero Jorge Humberto <a00573134@itesm.mx>
2023-02-23 11:16:37 -0600

    Prueba de porcentaje

--- projects/calculator/cypress/e2e/calc.cy.js
+++ projects/calculator/cypress/e2e/calc.cy.js
@@ -6,6 +6,17 @@ describe('template spec', () => {
     cy.contains('x').click()
     cy.contains('6').click()
     cy.get('.component-display').first().should('have.text', '6')
+  }),
+  it('pruebaDePorcentaje', () => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('AC').click()
+    cy.contains('5').click()
+    cy.contains('%').click()
+    cy.get('.component-display').first().should('have.text', '0.05')
+    cy.contains('%').click()
+    cy.get('.component-display').first().should('have.text', '0.0005')
+    cy.contains('%').click()
+    cy.get('.component-display').first().should('have.text', '0.000005')
   })
 })
 
