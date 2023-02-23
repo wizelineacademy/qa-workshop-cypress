60c732af767e1d3252c1038c00fb27a9ff9a0d04
EduardoHGz <88729982+LaloHGz@users.noreply.github.com>
2023-02-23 11:09:29 -0600

    caso prueba porcentaje

---
+++ projects/calculator/cypress/e2e/porc.cy.js
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+    it('passes', () => {
+      cy.visit('http://localhost:3000/')
+      cy.contains('AC').click()
+      cy.contains('2').click()
+      cy.contains('%').click()
+      cy.get('.component-display').first().should('have.text', '0.02')
+    })
+  })
