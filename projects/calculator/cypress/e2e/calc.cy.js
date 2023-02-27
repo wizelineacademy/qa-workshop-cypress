369c758878f7aa2ce33e8f780f650c3dfc67380d
Victoria Lucero R <70721302+VictoriaLR01@users.noreply.github.com>
2023-02-27 08:19:07 -0600

    Update calc.cy.js

--- projects/calculator/cypress/e2e/calc.cy.js
+++ projects/calculator/cypress/e2e/calc.cy.js
@@ -2,10 +2,10 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
     cy.contains('AC').click()
-    cy.contains('1').click()
+    cy.contains('2').click()
     cy.contains('0').click()
     cy.contains('0').click()
     cy.contains('%').click()
-    cy.get('.component-display').first().should('have.text', '0.1')
+    cy.get('.component-display').first().should('have.text', '0.2')
   })
 })
