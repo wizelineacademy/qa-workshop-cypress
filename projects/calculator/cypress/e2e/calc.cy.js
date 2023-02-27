55103d6b16a2a0067c3ba6e858aff08e925b2a33
Francisco Mestizo <A01731549@tec.mx>
2023-02-27 11:19:29 -0600

    Pruebas de porcentaje Francisco Mestizo

--- projects/calculator/cypress/e2e/calc.cy.js
+++ projects/calculator/cypress/e2e/calc.cy.js
@@ -1,3 +1,4 @@
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
@@ -6,5 +7,49 @@ describe('template spec', () => {
     cy.contains('x').click()
     cy.contains('6').click()
     cy.get('.component-display').first().should('have.text', '6')
+  })
+})
+
+describe('Prueba de porcentaje 1', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('AC').click()
+    cy.contains('1').click()
+    cy.contains('0').click()
+    cy.contains('%').click()
+    cy.contains('x').click()
+    cy.contains('5').click()
+    cy.contains('0').click()
+    cy.contains('=').click()
+    cy.get('.component-display').first().should('have.text', '5')
+  })
+})
+
+describe('Prueba de porcentaje 2', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('AC').click()
+    cy.contains('2').click()
+    cy.contains('%').click()
+    cy.contains('x').click()
+    cy.contains('1').click()
+    cy.contains('2').click()
+    cy.contains('=').click()
+    cy.get('.component-display').first().should('have.text', '0.24')
+  })
+})
+
+describe('Prueba de porcentaje 3', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('AC').click()
+    cy.contains('2').click()
+    cy.contains('0').click()
+    cy.contains('%').click()
+    cy.contains('x').click()
+    cy.contains('5').click()
+    cy.contains('0').click()
+    cy.contains('=').click()
+    cy.get('.component-display').first().should('have.text', '10')
   })
 })
