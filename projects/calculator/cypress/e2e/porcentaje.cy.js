b595c2db130692caa862eefe858483d209847e1d
Andrea Catalina Fernández Mena <katy.fernandezmena@gmail.com>
2023-02-28 22:46:41 -0600

    test A01197705

---
+++ projects/calculator/cypress/e2e/porcentaje.cy.js
@@ -0,0 +1,16 @@
+//Andrea Catalina Fernández Mena A01197795 
+
+describe('template spec', () => {
+    it('passes', () => {
+      cy.visit('http://localhost:3000/')
+      cy.contains('AC').click()
+      cy.contains('1').click()
+      cy.contains('3').click()  
+      cy.contains('%').click()
+      cy.contains('x').click()
+      cy.contains('0').click()
+      cy.contains('6').click()
+      cy.contains('=').click()
+      cy.get('.component-display').first().should('have.text', '6')
+    })
+  })
