ba7630f5f0f6a97dbe9ddd383f70c4c4e9dbe2a9
Gerardo Mora <gmorabeltran@gmail.com>
2023-02-28 22:33:12 -0600

    Unit Test Calculadora

---
+++ projects/calculator/cypress/e2e/percentTest.cy.js
@@ -0,0 +1,15 @@
+describe('template spec', () => {
+    it('passes', () => {
+      cy.visit('http://localhost:3000/')
+      cy.contains('AC').click()
+      cy.contains('2').click()
+      cy.contains('0').click()
+      cy.contains('%').click()
+      cy.contains('x').click()
+      cy.contains('7').click()
+      cy.contains('5').click()
+      cy.contains('0').click()
+      cy.contains('=').click()
+      cy.get('.component-display').first().should('have.text', '150')
+    })
+  })
