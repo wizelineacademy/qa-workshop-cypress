0b3f1b2920329a60a7b603beede759c00dcf0ad0
Carmina López <a00830265@itesm.mx>
2023-03-10 10:22:13 -0600

    Prueba falso, regla de tres y porcentaje

--- projects/calculator/cypress/e2e/por.cy.js
+++ projects/calculator/cypress/e2e/por.cy.js
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('Probar porcentaje 1', () => {
     it('passes', () => {
       cy.visit('http://localhost:3000/')
       cy.contains('AC').click()
@@ -9,7 +9,7 @@ describe('template spec', () => {
     })
 })
 
-describe('template spec', () => {
+describe('Probar porcentaje 2', () => {
     it('passes', () => {
       cy.visit('http://localhost:3000/')
       cy.contains('AC').click()
@@ -21,4 +21,43 @@ describe('template spec', () => {
       cy.contains('=').click()
       cy.get('.component-display').first().should('have.text', '0.5')
     })
+})
+
+describe('Porcentaje falso', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('AC').click()
+    cy.contains('5').click()
+    cy.contains('%').click()
+    cy.contains('x').click()
+    cy.contains('1').click()
+    cy.contains('=').click()
+    cy.get('.component-display').first().should('not.equal', '0.5')
+  })
+})
+
+Cypress.Commands.add('click_number',(number) => {
+  const aux = number.toString();
+  for (let i = 0; i < aux.length; i++){
+    cy.get('.component-button-panel').contains('div', aux[i]).click()
+  }
+})
+
+describe('Regla de tres, porcentaje', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+    // Numero random del 1 al 50 se guarda en una variable para el primer operando
+    const operando = 1 + Math.floor((Math.random() * 50))
+    // Numero random del 1 al 100 se guarda la variable para el operador porcentual
+    const porcentual = 1 + Math.floor((Math.random() * 100)) 
+    // Porcentaje con regla de tres
+    const resultado = (porcentual * operando) / 100
+    cy.contains('AC').click()
+    cy.click_number(operando)
+    cy.contains('x').click()
+    cy.click_number(porcentual)
+    cy.contains('%').click()
+    cy.contains('=').click()  
+    cy.get('.component-display').first().contains(resultado)
+  })
 })
