52412d453ac51ace227cf273a8739f9b9a4b3780
Victoria Lucero R <70721302+VictoriaLR01@users.noreply.github.com>
2023-03-12 22:00:26 -0600

    pruebas

--- projects/calculator/cypress/e2e/calc.cy.js
+++ projects/calculator/cypress/e2e/calc.cy.js
@@ -7,5 +7,47 @@ describe('template spec', () => {
     cy.contains('0').click()
     cy.contains('%').click()
     cy.get('.component-display').first().should('have.text', '0.2')
-  })
+
+    it('porcentaje', () => {
+      cy.visit('http://localhost:3000/')
+      cy.contains('AC').click()
+      cy.contains('5').click()
+      cy.contains('%').click()
+      cy.get('.component-display').first().should('have.text', '0.05')
+      cy.contains('%').click()
+      cy.get('.component-display').first().should('have.text', '0.0005')
+      cy.contains('%').click()
+      cy.get('.component-display').first().should('have.text', '0.000005')
+    }),
+    it('false', () => {
+      cy.visit('http://localhost:3000/')
+      cy.contains('AC').click()
+      cy.contains('2').click()
+      cy.contains('+').click()
+      cy.contains('5').click()
+      cy.get('.component-display').first().should('not.have.text', '7')
+    }),
+    it('numRandom', () => {
+      let numu = random(1, 50);
+      let dos = random(1, 100);
+      let result = (dos * numu) / 100;
+
+      cy.visit('http://localhost:3000/')
+      cy.contains('AC').click()
+      cy.contains(Math.floor((dos/100)%10)).click()
+      cy.contains(Math.floor((dos/10)%10)).click()
+      cy.contains(Math.floor(dos%10)).click()
+      cy.contains('%').click()
+      cy.contains('x').click()
+      cy.contains(Math.floor((numu/10)%10)).click()
+      cy.contains(Math.floor(numu%10)).click()
+      cy.contains('=').click()
+      cy.get('.component-display').first().should('have.text', result)
+
+    })
 })
+
+function random(min, max) {
+  return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+  })
