d81e954f4d6462d9375f854d46b62dccdd564699
MarioR4 <84197741+MarioR4@users.noreply.github.com>
2023-03-12 20:24:18 -0600

    Prueba falsa y Prueba de regla de 3

--- projects/calculator/cypress/e2e/percent.cy.js
+++ projects/calculator/cypress/e2e/percent.cy.js
@@ -1,3 +1,25 @@
+
+var num1 = Math.floor(Math.random() * 51);
+let inp1 = num1.toString();
+var num2 = Math.floor(Math.random() * 101);
+let inp2 = num2.toString();
+var numResult = (num2 * num1) / 100;
+
+function setDigits(num, inp) {
+    if (num == 100) {
+      cy.contains('1').click()
+      cy.contains('0').click()
+      cy.contains('0').click()
+    }
+    else if (num >= 10) {
+      cy.contains(inp.charAt(0)).click()
+      cy.contains(inp.charAt(1)).click()
+    }
+    else {
+      cy.contains(inp).click()    
+    }
+}
+
 describe('template spec', () => {
     it('passes', () => {
       cy.visit('http://localhost:3000/')
@@ -11,4 +33,28 @@ describe('template spec', () => {
       cy.contains('=').click()
       cy.get('.component-display').first().should('have.text', '6')
     })
-  })
+
+    it('not passes', () => {
+      cy.visit('http://localhost:3000/')
+      cy.contains('AC').click()
+      cy.contains('1').click()
+      cy.contains('0').click()
+      cy.contains('%').click()
+      cy.contains('x').click()
+      cy.contains('8').click()
+      cy.contains('0').click()
+      cy.contains('=').click()
+      cy.get('.component-display').first().should('have.text', '6')
+    })
+
+    it('rule of three', () => {
+      cy.visit('http://localhost:3000/')
+      cy.contains('AC').click()
+      setDigits(num2, inp2)
+      cy.contains('%').click()
+      cy.contains('x').click()
+      setDigits(num1, inp1)
+      cy.contains('=').click()
+      cy.get('.component-display').first().should('have.text', numResult)
+    })
+})
